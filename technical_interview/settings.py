7336314bee90dbda9339bbf26aa7b3824e627524
unknown <mainadanielchiwaz@gmail.com>
2022-04-09 03:53:37 +0300

    second commit

--- technical_interview/settings.py
+++ technical_interview/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#b+t6b)lel792%-1$3brhn+)bnmqoyfrynsn&4ig!4r^fvgbmz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['162.243.165.51',]
 
 
 # Application definition
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/static/")
